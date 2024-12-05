d851fe666154d625cb3c0a0b1e4de1768a433845
Ashikmahbub <ashikmahbub56@gmail.com>
2024-12-05 12:30:57 +0600

    index.js modified

--- index.js
+++ index.js
@@ -27,6 +27,5 @@ app.use("/api", manageOrderRoutes);
 // Serve static files from the 'uploads' folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Export the app for Vercel
+module.exports = app;
