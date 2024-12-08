b4c4944721e48e2d5e0b99039bdf7f9cec782bf3
Ashikmahbub <ashikmahbub56@gmail.com>
2024-12-08 13:55:26 +0600

    vercel json added

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+// const express = require("express");
+// const cors = require("cors");
+// require("dotenv").config();
+// const path = require("path");
+// const productManagementRoutes = require("./routes/productManagementRoutes");
+// const getProductRoutes = require("./routes/getProductRoutes");
+// const createOrderRoutes = require("./routes/createOrderRoutes");
+// const salesRoutes = require("./routes/salesRoutes");
+// const manageOrderRoutes = require("./routes/manageOrdersRoutes");
+// const { connectToDatabase } = require("./config/dbConnection"); // Importing the dbConnection
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// // Connect to the database
+// connectToDatabase();  
+
+// // API routes
+// app.use("/api", productManagementRoutes);
+// app.use("/api", getProductRoutes);
+// app.use("/api", createOrderRoutes);
+// app.use("/api", salesRoutes);
+// app.use("/api", manageOrderRoutes);
+
+// // Serve static files from the 'uploads' folder
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// // Start the server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -27,6 +59,5 @@ app.use("/api", manageOrderRoutes);
 // Serve static files from the 'uploads' folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Export the app for Vercel
+module.exports = app;
